de4c37a2fd43490746e340ce90ba4b77d193f145
AleksaderZarnowski <azarnowski1@gmail.com>
2013-11-22 02:22:32 +0100

    Added simple GUI

--- FurnaceDriver/exe/DefaultConfig/GUIAdapter.java
+++ FurnaceDriver/exe/DefaultConfig/GUIAdapter.java
@@ -4,28 +4,59 @@ import Default.decrease_button_click;
 import Default.fan_button_click;
 import Default.increase_button_click;
 import Default.menu_button_click;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.border.LineBorder;
 
 public class GUIAdapter {
 	private LCDDisplay display;
-	
+	private JPanel[] diods;
+	private JTextArea displayArea = new JTextArea();
+
+	public void setTextArea(JTextArea area) {
+		this.displayArea = area;
+	}
+
 	public void setDisplay(LCDDisplay display){
 		this.display = display;
 	}
 	
 	public void displayEverything(){
-		System.out.println("Fan led :" + getFanLed());
-		System.out.println("Fan off/on led :" + getFanOffLed());
+		if (getFanLed()) 
+			diods[2].setBackground(Color.red);
+		else 
+			diods[2].setBackground(Color.white);
+		if(getFanOffLed())
+			diods[4].setBackground(Color.red);
+		else 
+			diods[4].setBackground(Color.white);
 		System.out.println("Fan blinking led :" + getFanLedBlinking());
-		System.out.println("Temp led :" + getTemperatureLed());
+		if (getTemperatureLed()) 
+			diods[3].setBackground(Color.red);
+		else 
+			diods[3].setBackground(Color.white);
 		
-		System.out.println("Display:");
-		System.out.print(getFirstCharacter());
-		System.out.print(getSecondCharacter());
-		System.out.print(getThirdCharacter());
-		System.out.print(getFourthCharacter());
-		System.out.print(getFifthCharacter());
-		System.out.println("");
+		StringBuffer buffer = new StringBuffer();
+        buffer.append(getFirstCharacter()).append(" : ").append(getSecondCharacter())
+        	.append(" : ").append(getThirdCharacter()).append(" : ")
+        	.append(getFourthCharacter()).append(" : ").append(getFifthCharacter());
+        displayArea.setText(buffer.toString());
+	}
+
+	public void setDiods(JPanel[] diods) {
+		this.diods = diods;
 	}
 	
 	public boolean getHoursLed(){
--- FurnaceDriver/exe/DefaultConfig/MainDefaultComponentMod.java
+++ FurnaceDriver/exe/DefaultConfig/MainDefaultComponentMod.java
@@ -15,14 +15,28 @@ import Default.*;
 import com.ibm.rational.rhapsody.oxf.*;
 //## auto_generated
 import com.ibm.rational.rhapsody.animcom.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.border.LineBorder;
 //----------------------------------------------------------------------------
 // MainDefaultComponentMod.java                                                                  
 //----------------------------------------------------------------------------
 
 
 //## ignore 
-public class MainDefaultComponentMod {
+public class MainDefaultComponentMod extends JFrame {
     
     //#[ ignore
     // link with events in order to register them in the animation browser
@@ -106,29 +120,156 @@ public class MainDefaultComponentMod {
     
     protected static Dispatcher p_Dispatcher = null;
     static DataProvider receptor;
+    static GUIAdapter guiAdapter;
+
+    private static final int DIOD_SIZE = 20;
+    private JPanel mainPanel = new JPanel();
+    private JPanel buttonsPanel = new JPanel();
+    private JPanel diodsPanel = new JPanel();
+
+    private JLabel[] diods_desc = new JLabel[5];
+    private static JPanel[] diods = new JPanel[5];
+    private JButton menuButton = new JButton("MENU");
+    private JButton minusButton = new JButton("Minus");
+    private JButton plusButton = new JButton("Plus");
+    private JButton clockButton = new JButton("Clock");
+    private JButton fanButton = new JButton("Fan");
+    private static JTextArea displayArea = new JTextArea();
+
+    public MainDefaultComponentMod() {
+        initGUI();
+    }
+    
+    private void initGUI() {
+        mainPanel.setLayout(new BorderLayout(10, 10));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        initializeDiods();
+        setPanels();
+        setComponents();
+
+        add(mainPanel);
+        pack();
+
+        setTitle("Furnace Driver");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+
+    private void setPanels() {
+        buttonsPanel.setLayout(new GridLayout(1, 5, 5, 5));
+        buttonsPanel.add(menuButton);
+        buttonsPanel.add(minusButton);
+        buttonsPanel.add(plusButton);
+        buttonsPanel.add(clockButton);
+        buttonsPanel.add(fanButton);
+
+        mainPanel.add(buttonsPanel, BorderLayout.PAGE_END);
+
+        diodsPanel.setLayout(new GridLayout(2, 5, 5, 5));
+        for (JLabel label : diods_desc) {
+            diodsPanel.add(label);
+        }
+        for (JPanel diod : diods) {
+            diodsPanel.add(diod);
+        }
+
+        mainPanel.add(diodsPanel, BorderLayout.PAGE_START);
+    }
+
+    private void setComponents() {
+        mainPanel.add(displayArea, BorderLayout.CENTER);
+        displayArea.setEditable(false);
+        
+        initializeButtons();
+    }
+
+    private void initializeDiods() {
+        diods[0] = new JPanel();
+        diods[1] = new JPanel();
+        diods[2] = new JPanel();
+        diods[3] = new JPanel();
+        diods[4] = new JPanel();
+
+        for (JPanel diod : diods) {
+            diod.setPreferredSize(new Dimension(DIOD_SIZE, DIOD_SIZE));
+            diod.setBorder(LineBorder.createBlackLineBorder());
+            diod.setBackground(Color.white);
+        }
+
+        diods_desc[0] = new JLabel("HOURS_LED");
+        diods_desc[1] = new JLabel("MINUTES_LED");
+        diods_desc[2] = new JLabel("FAN_LED");
+        diods_desc[3] = new JLabel("TEMP_LED");
+        diods_desc[4] = new JLabel("FAN_OFF_LED");
+    }
+    
+    private void initializeButtons() {
+        menuButton.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                guiAdapter.menuButtonClick();
+            }
+        });
+        
+        plusButton.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                guiAdapter.increaseButtonClick();
+            }
+        });
+        
+        minusButton.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                guiAdapter.decreaseButtonClick();
+            }
+        });
+        
+        clockButton.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                guiAdapter.clockButtonClick();
+
+            }
+        });
+        
+        fanButton.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                guiAdapter.fanButtonClick();
+            }
+        });
+    }
+
     
     //## configuration DefaultComponent::Animation 
     public static void main(String[] args) {
-        System.out.println("1");
         RiJOXF.Init(null, 0, 0, true, args);
-        System.out.println("2");
         MainDefaultComponentMod initializer_DefaultComponent = new MainDefaultComponentMod();
-        System.out.println("3");
+        initializer_DefaultComponent.setVisible(true);
         receptor = (DataProvider) new DataProviderImpl();
        
         System.out.println( "Wyslano interfejs" );
-        p_Dispatcher = new Dispatcher(RiJMainThread.instance());
-        System.out.println("MAIN");
-        p_Dispatcher.setDataProvider(receptor);
-        System.out.println("MAIN");
-        p_Dispatcher.startBehavior();
+        
         //#[ configuration DefaultComponent::Animation 
         //#]
         System.out.println("MAIN");
 				new Thread()
         {
             public void run() {
-			    GUIAdapter guiAdapter = new GUIAdapter();
+			    guiAdapter = new GUIAdapter();
+                guiAdapter.setDiods(diods);
+                guiAdapter.setTextArea(displayArea);
+                p_Dispatcher = new Dispatcher(RiJMainThread.instance());
+                p_Dispatcher.setDataProvider(receptor);
+                p_Dispatcher.startBehavior();
 				guiAdapter.setDisplay(p_Dispatcher.getItsLCDDisplay());
             	while(true){
 					try{
